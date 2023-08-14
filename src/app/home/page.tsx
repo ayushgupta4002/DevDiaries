b8ea8f3db4a131e2324f52cad28abb0b58115b9d
Ayush <ayush4002gupta@gmail.com>
2023-08-14 16:35:12 +0530

    Navbar component added, home route added ;

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../Components/navbar";
+function home() {
+  return (
+    <div className="min-h-screen">
+    </div>
+  );
+}
+
+export default home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,39 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+    
+      'xl': {'max': '1380px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1103px'},
+      // => @media (max-width: 1103px) { ... }
+      'xllg': {'max': '1146px'},
+
+      'xlg': {'max': '1243px'},
+
+      'mlg': {'max': '1025px'},
+      // => @media (max-width: 1103px) { ... }
+
+
+      'xmd': {'max': '900px'},
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+      'nm': {'max': '680px'},
+
+      'xnm':{'max':'860px'},
+
+      'sm': {'max': '580px'},
+
+      // => @media (max-width: 639px) { ... }
+      'xsml': {'max': '556px'},
+
+      'xsm': {'max': '380px'},
+      'xssm': {'max': '340'},
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
